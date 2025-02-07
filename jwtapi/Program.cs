0848ccbcc01bdaa53cd98bc434ba667b82093503
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-06 20:27:24 -0500

    Add new routes for protected endpoints and user authentication
    Introduce routes for handling protected endpoints and user authentication:
    - Add `/protected` route requiring authorization, returning a message with the user's name.
    - Add `/protectedwithscope` route requiring a specific claim (`scope` with value `myapi:drunken`), returning a message with the user's name and scope.
    - Add `/login/{user}/{password}` route for user login, generating a JWT token with claims for the user's name and scope, and an expiration time of one hour if credentials match predefined values (`pato` and `donal`).

--- jwtapi/Program.cs
+++ jwtapi/Program.cs
@@ -29,5 +29,36 @@ builder.Services.AddAuthentication("Bearer")
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/protected", (ClaimsPrincipal user) => "Route protected! User: " + user.Identity?.Name)
+  .RequireAuthorization();
+
+app.MapGet("/protectedwithscope", (ClaimsPrincipal user) => $"Route protected with scope! User: {user.Identity?.Name} - Scope: {user.FindFirst("scope")?.Value}")
+  .RequireAuthorization(p => p.RequireClaim("scope", "myapi:drunken")); // el scope es como tener roles en la aplicación
+
+app.MapGet("/login/{user}/{password}", (string user, string password) => {
+  if (user == "pato" &&  password == "donal")
+  {
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var byteKey = Encoding.ASCII.GetBytes(key);
+    var tokenDes = new SecurityTokenDescriptor
+    {
+      Subject = new ClaimsIdentity(new Claim  []
+      {
+        new Claim(ClaimTypes.Name, user), // el claim recibe un string con el nombre del claim (puede ser cualquier cosa) y el valor
+        new Claim("scope", "myapi:drunken")
+      }), // los claims son los datos que se van a guardar en el token
+      Expires = DateTime.UtcNow.AddHours(1), // tiempo de expiración del token -> AddDays, AddMinutes, AddMonths, etc.
+      SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(byteKey), SecurityAlgorithms.HmacSha256Signature) // la misma que usamos en AddJwtBearer
+    };
+
+    var token = tokenHandler.CreateToken(tokenDes);
+
+    return Results.Ok(tokenHandler.WriteToken(token)); // retorna el token generado como un string
+  } 
+  else
+  {
+    return Results.Unauthorized();
+  }
+});
 
 app.Run();
